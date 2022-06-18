35e38ab6a3e5d205e691ff9b6bbf4484b04c948c
Olga Nagornaya <oinagornaya@gmail.com>
2022-06-18 13:06:50 +0300

    Итоговая задача - теперь красиво

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,13 +38,14 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {
         client: '', 
         order: {
-              address: address,
-              sum: sum,
+            //  address: address,
+            //   sum: sum,
         }, 
-        goods: [{
-              title: this.title,
-              count: this.count,
-            }    
+        goods: [
+            // {
+            //  title: this.title,
+            //  count: this.count,
+            //}    
         ]
     }
 
@@ -53,7 +54,7 @@ function sendRequest(name, phone, address, goods, sum) {
     for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title:goods[i].title, count:goods[i].count});
     }
-    data.goods.shift();
+   // data.goods.shift();
         
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
